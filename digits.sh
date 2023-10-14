2d6b02a27e6eb0c01af619f8d39c60709a80a792
kai cui <kcui23@wisc.edu>
2023-10-14 18:15:00 -0500

    Complete problem 4.

--- digits.sh
+++ digits.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+numbers=$(echo {1000..2000})
+sum=0
+
+for i in $numbers; do
+  if [[ $i =~ ^[01]+$ ]]; then 
+      sum=$(($sum + $i))
+  fi
+done
+echo "The sum of the numbers between 1000 and 2000 (inclusive) having digits only from the set {0, 1} is $sum."
