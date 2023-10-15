e7e0e5616822c5df689a1048f7a8f889cfac5da2
Ziyi Song <zsong252@wisc.edu>
2023-10-14 23:05:07 -0500

    command added by zsong252

--- school.sh
+++ school.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+echo "The sum is "
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | awk '{ total+=$1 } END { print total }' #find the sum
+echo "The average is "
+head Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | awk '{ total+=$1 } END { print total/NR }' #find the average
+
+
