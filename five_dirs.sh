046c4b6f74bb28589ec0254a7398bcae5bde2de7
ding247 <ding247@wisc.edu>
2023-10-15 21:50:13 -0500

    upload!

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+mkdir five
+for i in {1..5}
+do
+    mkdir "five/dir$i"
+done
+ 
+for dir in {1..5}; do
+	   for file_num in {1..4}; do
+	       for line in $(seq 1 $file_num); do
+		   echo $file_num >> "five/dir$dir/file$file_num"
+	       done
+	   done
+	done
+   
