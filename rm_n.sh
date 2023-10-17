8c4bb626781c1267dbe114d54acb0ead3a66466d
Baiheng Chen <bchen342@wisc.edu>
2023-10-17 13:51:43 -0500

    add rm

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+if [[ $# -ne 2 ]]; then
+    echo 2 inputs are needed: first one is dictionary, the second is the number of bytes 1>&2
+    exit 1
+fi
+dir=$1
+n=$2
+find "$dir" -type f -size +"$n"c -exec rm {} \;
+echo "Files larger than $n bytes in directory $dir have been removed."
+
