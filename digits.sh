2be76d875ada99e1df00742f121b5eb6fd1677e4
kai cui <kcui23@wisc.edu>
2023-10-19 13:16:55 -0500

    correct outputs

--- digits.sh
+++ digits.sh
@@ -8,4 +8,4 @@ for i in $numbers; do
       sum=$(($sum + $i))
   fi
 done
-echo "The sum of the numbers between 1000 and 2000 (inclusive) having digits only from the set {0, 1} is $sum."
+echo $sum
